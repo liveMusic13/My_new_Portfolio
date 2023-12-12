749dae1fee7d9b16a2a5286261f1c5ec660f4dd8
Dmitriy <aegis.ko20@mail.ru>
2023-12-12 19:50:37 +0300

    save

--- src/hooks/useAuthors.ts
+++ src/hooks/useAuthors.ts
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import { isAuthorContext } from '../providers/isAuthorProvider';
+import { isAuthorContext } from '../providers/IsAuthorProvider';
 import { IAuthor } from '../types/isAuthor.types';
 
 export const useAuthors = (): IAuthor => useContext(isAuthorContext);
