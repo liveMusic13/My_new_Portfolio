5f466c618dd8a01db948ce24c62b8e3c5daf1033
Dmitriy <aegis.ko20@mail.ru>
2023-12-12 16:46:31 +0300

    fix

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -2,7 +2,15 @@ import { FC } from 'react';
 import styles from './Contact.module.scss';
 
 const Contact: FC = () => {
-	return <div className={styles.wrapper}>Contact</div>;
+	return (
+		<div className={styles.wrapper}>
+			<div className={styles.block__exit}></div>
+			<a href='tel:+375255376431'>Телефон</a>
+			<a href='mailto:aegis.ko20@mail.ru'>Почта</a>
+			<a href='https://t.me/DmitriyKovalenko13'>Телеграм</a>
+			<a href='https://vk.com/id165406586'>ВКонтакте</a>
+		</div>
+	);
 };
 
 export default Contact;
